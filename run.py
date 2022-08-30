d85d8c8b4fc899c997dc7b28c89c2df54c6d81d0
Roy Zheng <royz@23andme.com>
2022-08-30 14:24:55 -0700

    Refactor into PriceTrends

--- run.py
+++ run.py
@@ -12,23 +12,58 @@ STARTING_CASH = 100
 GRAPH_MAX_WIDTH = 80
 
 
+class PriceTrends(object):
+    STABLE = 0
+    CRASH = 1
+    POP = 2
+    UP = 3
+    DOWN = 4
+
+    def __init__(self):
+        self.price_time = time.time()
+        self.trend_time = time.time()
+        self.trend_up = random.randint(0, 1)
+        self._buy = 5
+        self._sell = 5.01
+        self.price_history = collections.deque()
+
+    @property
+    def buy(self):
+        return self._buy
+
+    @property
+    def sell(self):
+        return self._sell
+
+    def update_price(self):
+        if self.price_time > time.time():
+            return
+
+        if self.trend_time < time.time():
+            self.trend_time = time.time() + random.uniform(5, 15)
+            self.trend_up = random.randint(0, 1)
+        change = round(random.uniform(-0.03 if not self.trend_up else -0.02, 0.03 if self.trend_up else 0.02), 2)
+
+        self._buy += change
+        self._sell = self.buy + float(random.randrange(1, 2)) / 100
+        self.price_time = time.time() + float(random.randrange(5, 20)) / 10
+        self.price_history.append(self.buy)
+        if len(self.price_history) > GRAPH_MAX_WIDTH:
+            self.price_history.popleft()
+
+
 class ShavingGame(object):
 
     def __init__(self):
         self.cash = STARTING_CASH
-        self.buy = 5
-        self.sell = 5.01
         self.owned = 0
         self.debug_str = ''
         self.render_time = time.time()
-        self.price_time = time.time()
-        self.trend_time = time.time()
-        self.trend_up = 1
         self.buy_limits = collections.defaultdict(lambda:0)
         self.sell_limits = collections.defaultdict(lambda:0)
-        self.price_history = collections.deque()
+        self.price_mgr = PriceTrends()
 
-    def draw_graph(self, start_index, values, height=10):
+    def draw_graph(self, values, height=10):
         lines = ['Price History']
         if not values:
             return lines
@@ -43,23 +78,18 @@ class ShavingGame(object):
             line = '{0:>80}'.format(line)
             lines.append(line)
         return lines
+
+    @property
+    def sell(self):
+        return self.price_mgr.sell
+
+    @property
+    def buy(self):
+        return self.price_mgr.buy
         
     def update_price(self):
-        if self.price_time > time.time():
-            return
-        if self.trend_time < time.time():
-            self.trend_time = time.time() + random.uniform(5, 15)
-            self.trend_up = random.randint(0, 1)
-        change = round(random.uniform(-0.03 if not self.trend_up else -0.02, 0.03 if self.trend_up else 0.02), 2)
+        self.price_mgr.update_price()
 
-        self.debug_str = "change={}trend_up={}".format(change, self.trend_up)
-        self.buy += change
-        self.sell = self.buy + float(random.randrange(1,2))/100
-        self.price_time = time.time() + float(random.randrange(5,20))/10
-        self.price_history.append(self.buy)
-        if len(self.price_history) > GRAPH_MAX_WIDTH:
-            self.price_history.popleft()
-        
         for price in (price for price in self.buy_limits if price >= self.sell):
             self.owned += self.buy_limits[price]
             self.buy_limits[price] = 0
@@ -90,18 +120,23 @@ class ShavingGame(object):
                 for price in self.sell_limits:
                     self.owned += self.sell_limits[price]
                     self.sell_limits[price] = 0
-                
+
+    @property
+    def value(self):
+        return (self.cash + self.owned*self.buy
+                + sum(k * v for k, v in self.buy_limits.items())
+                + sum(k*v for k, v in self.sell_limits.items()))
+
     def run(self, screen):
         screen.nodelay(True)
         while True:
             if time.time() < self.render_time + REFRESH_TIME:
                 time.sleep((self.render_time + REFRESH_TIME) - time.time())
             screen.erase()
-            value = self.cash + self.owned*self.buy
             output = [
                 'Cash: ${:,.2f}      Shares: {}'.format(self.cash, self.owned),
                 'Buy: ${:,.2f}       Sell: {:,.2f}'.format(self.buy, self.sell),
-                'Value: ${:,.2f}      Gain: {:,.1f}%'.format(value, ((value - STARTING_CASH) / STARTING_CASH)*100),
+                'Value: ${:,.2f}      Gain: {:,.1f}%'.format(self.value, ((self.value - STARTING_CASH) / STARTING_CASH)*100),
                 'Debug: {}'.format(self.debug_str),
             ]
             output.append('{:<40}{:>40}'.format('Limit Buy Orders', 'Limit Sell Orders'))
@@ -117,13 +152,14 @@ class ShavingGame(object):
                 if sell_tup:
                     sell_msg = "${:,.2f} - {} shares".format(sell_tup[0], sell_tup[1])
                 output.append('{:<40}{:>40}'.format(buy_msg, sell_msg))
-            output += self.draw_graph(len(output), self.price_history)
+            output += self.draw_graph(self.price_mgr.price_history)
             for i, s in enumerate(output):
                 screen.addstr(i, 0, s)
             self.render_time = time.time()
             self.process_input(screen)
             self.update_price()
-            if time.time() <= self.price_time:
+
+            if time.time() <= self.price_mgr.price_time:
                 self.update_price()
             # self.debug_str = 'trend_up={}'.format(self.trend_up)
 
